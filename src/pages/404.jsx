30a85cd9fc1b1d126a42a41016a1cc945f82c718
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-03-13 13:13:14 +0700

    implement head

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 const error = () => {
     return (
         <div>
+            <Helmet>
+                <title>Opps, error</title>
+            </Helmet>
             <p>Error</p>
         </div>
     )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 const index = () => {
     return (
         <div>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
             <p>Hello World</p>
         </div>
     )
