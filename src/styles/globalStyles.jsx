2a6f96d090edd166c8b001a200fbf509dbdd6809
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-03-13 13:26:08 +0700

    edit global styles

--- src/styles/globalStyles.jsx
+++ src/styles/globalStyles.jsx
@@ -6,28 +6,6 @@ const globalStyles = createGlobalStyle`
         margin : 0;
         box-sizing : border-box;
     }
-
-    body {
-        background-color: ${props => props.theme.body};
-    }
-
-    ::-webkit-scrollbar {
-        width: 5px;
-        height: 5px;
-    }
-
-    ::-webkit-scrollbar-track {
-        background: ${props => props.theme.body};
-        transition : 0.5s;
-    }
-
-    ::-webkit-scrollbar-thumb {
-        background: ${props => props.theme.yellow}; 
-    }
-
-    ::-webkit-scrollbar-thumb:hover {
-        background: #555; 
-    }
 `
 
 export default globalStyles
