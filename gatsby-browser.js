d87e51c98a77a1611bed79a740e3b2e1a626d6c3
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-03-13 13:35:19 +0700

    edit gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import { GlobalStyles } from 'Styles'
 
 const wrapRootElement = ({ element }) => (
-    <>
+    <Fragment>
         <GlobalStyles />
         {element}
-    </>
+    </Fragment>
 )
 
 export { wrapRootElement }
